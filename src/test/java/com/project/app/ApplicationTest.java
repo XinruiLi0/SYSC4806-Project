6a0556c113332b99d7906592cca9ec06e5bb900c
xilingdc <55708629+xilingdc@users.noreply.github.com>
2021-03-11 11:40:28 -0500

    tests

---
+++ src/test/java/com/project/app/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.project.app;
+
+import com.project.app.Questionnaire;
+import org.junit.Test;
+import org.testng.Assert;
+
+public class ApplicationTest {
+
+
+}
---
+++ src/test/java/com/project/app/QuestionnaireTest.java
@@ -0,0 +1,44 @@
+package com.project.app;
+
+import com.project.app.Questionnaire;
+import org.junit.Test;
+import org.testng.Assert;
+
+public class QuestionnaireTest {
+
+    @Test
+    public void InitializationQuestionnaire(){
+        Questionnaire q = new Questionnaire("Jake","jake@gmail.com",false,false,null,false);
+        Assert.assertEquals(q.getName(),"Jake");
+        Assert.assertEquals(q.getEmail(),"jake@gmail.com");
+        Assert.assertEquals(q.isRemainInResidence(),false);
+        Assert.assertEquals(q.isNeedSupport(),false);
+        Assert.assertEquals(q.isExperienceSymptoms(),false);
+    }
+
+    @Test
+    public void ifNeedSupportTest(){
+        Questionnaire q = new Questionnaire("Jake","jake@gmail.com",false,false,"food",false);
+        Assert.assertFalse(q.ifNeedSupport());
+        Assert.assertEquals(q.getSupportType(),null);
+
+        Questionnaire q1 = new Questionnaire("Jake","jake@gmail.com",false,true,"food",false);
+        Assert.assertTrue(q1.ifNeedSupport());
+        Assert.assertEquals(q1.getSupportType(),"food");
+    }
+
+    @Test
+    public void toStringTest(){
+        Questionnaire q = new Questionnaire("Jake","jake@gmail.com",false,false,null,false);
+        String str = "Name: " + "Jake" + "\n" + " Support Type: "+ "no need support \n" + " If experiencing Symptoms: " + " no symptoms \n";
+        Assert.assertEquals(q.toString(), str);
+
+        Questionnaire q1 = new Questionnaire("Jake","jake@gmail.com",false,true,"food",false);
+        String str1 = "Name: " + "Jake" + "\n" + " Support Type: "+ "food\n" + " If experiencing Symptoms: " + " no symptoms \n";
+        Assert.assertEquals(q1.toString(), str1);
+
+        Questionnaire q2 = new Questionnaire("Jake","jake@gmail.com",false,true,"food",true);
+        String str2 = "Name: " + "Jake" + "\n" + " Support Type: "+ "food\n" + " If experiencing Symptoms: " + "yes \n";
+        Assert.assertEquals(q2.toString(), str2);
+    }
+}
