fe207626006c27657bab5de48da23c5d37db8ad0
ChenZzz <55708302+zewenchen0719@users.noreply.github.com>
2021-03-12 02:10:21 -0500

    Debugging#5

--- src/test/java/Web/QuestionnaireTest.java
+++ src/test/java/Web/QuestionnaireTest.java
@@ -1,8 +1,15 @@
 package Web;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testng.Assert;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 public class QuestionnaireTest {
 
     @Test
