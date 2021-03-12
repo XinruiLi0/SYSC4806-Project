e091b5da77023ef51822cace180190e18f0018a9
xilingdc <55708629+xilingdc@users.noreply.github.com>
2021-03-11 20:15:54 -0500

    javadocs

--- src/main/java/com/project/app/Questionnaire.java
+++ src/main/java/com/project/app/Questionnaire.java
@@ -19,58 +19,112 @@ public class Questionnaire {
     private String email;
 
 
+    /**
+     * name getter
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * name setter
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * email getter
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * email setter
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * experienceSymptoms getter
+     * @return true if the patient has Symptoms, false if the patient does not have Symptoms
+     */
     public boolean isExperienceSymptoms() {
         return experienceSymptoms;
     }
 
+    /**
+     * experienceSymptoms setter
+     * @param experienceSymptoms
+     */
     public void setExperienceSymptoms(boolean experienceSymptoms) {
         this.experienceSymptoms = experienceSymptoms;
     }
 
+    /**
+     * remainInResidence getter
+     * @return remainInResidence
+     */
     public boolean isRemainInResidence() {
         return remainInResidence;
     }
 
+    /**
+     * remainInResidence setter
+     * @param remainInResidence
+     */
     public void setRemainInResidence(boolean remainInResidence) {
         this.remainInResidence = remainInResidence;
     }
 
+    /**
+     * needSupport getter
+     * @return needSupport
+     */
     public boolean isNeedSupport() {
         return needSupport;
     }
 
+    /**
+     * needSupport setter
+     * @param needSupport
+     */
     public void setNeedSupport(boolean needSupport) {
         this.needSupport = needSupport;
     }
 
+    /**
+     * supportType getter
+     * @return supportType
+     */
     public String getSupportType() {
         return supportType;
     }
 
+    /**
+     * supportType setter
+     * @param supportType
+     */
     public void setSupportType(String supportType) {
         this.supportType = supportType;
     }
 
 
-
-
-
+    /**
+     * constructor
+     * @param name
+     * @param email
+     * @param remainInResidence
+     * @param needSupport
+     * @param supportType
+     * @param experienceSymptoms
+     */
     public Questionnaire(String name, String email, boolean remainInResidence, boolean needSupport, String supportType, boolean experienceSymptoms ){
         this.name = name;
         this.email = email;
@@ -80,19 +134,34 @@ public class Questionnaire {
         this.supportType = supportType;
     }
 
+    /**
+     * default constructor
+     */
     public Questionnaire(){
 //        this("unknown","unknown",true,false,null,"none");
     }
 
 
+    /**
+     * Id getter
+     * @return id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Id setter
+     * @param id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * needSupport getter, if needSupport = false, the supportType would become null
+     * @return needSupport
+     */
     public Boolean ifNeedSupport(){
         if(!needSupport){
             this.setSupportType(null);
@@ -102,7 +171,10 @@ public class Questionnaire {
     }
 
 
-
+    /**
+     * toString method
+     * @return String
+     */
     public String toString(){
          String s = "Name: " + name + "\n" + " Support Type: ";
         if(ifNeedSupport()){
--- src/main/java/com/project/app/WebController.java
+++ src/main/java/com/project/app/WebController.java
@@ -11,12 +11,23 @@ public class WebController {
     @Autowired
     QuestionnaireRepo QRepo;
 
+    /**
+     * initialize Questionnaire
+     * @param model
+     * @return
+     */
     @GetMapping("/questionnaire")
     public String questionnaire(Model model){
         model.addAttribute("Questionnaire",new Questionnaire());
         return "questionnaire";
     }
 
+    /**
+     * save
+     * @param ques
+     * @param model
+     * @return
+     */
     @ResponseBody
     @PostMapping(value = "/questionnaire")
     public Questionnaire questionnaireForm(@ModelAttribute Questionnaire ques, Model model){
@@ -25,7 +36,11 @@ public class WebController {
         return ques;
     }
 
-
+    /**
+     * search email and send corresponding information
+     * @param email
+     * @return Questionnaire toString
+     */
     @GetMapping("/result")
     @ResponseBody
     public String showResult(String email){
