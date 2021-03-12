493f75a157c414976787da5a9f1fdf797497bb1e
XinruiLi0 <xinruili3@cmail.carleton.ca>
2021-03-12 00:39:46 -0500

    fix: Restore to previous commit

--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -1,61 +1,63 @@
 package app;
 
-import org.junit.jupiter.api.Test;
 
+import app.Questionnaire;
+import app.QuestionnaireRepo;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class ApplicationTest {
 
+    @Mock
+    QuestionnaireRepo QR;
+
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void contextLoads() {
+    public void getQuestionnaireTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/questionnaire");
+        mvc.perform(request).andDo(print()).andExpect(content().string(containsString("COVID-19 Questionaire")));
+
     }
 
     @Test
-    public void getQuestionnaireTest() throws Exception {
-        this.mvc.perform(get("/questionnaire")).andDo(print()).andExpect(content().string(containsString("COVID-19 Questionaire")));
+    public void postQuestionnaireTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.post("/questionnaire");
+        mvc.perform(request).andDo(print()).andExpect(content().string(equalTo(
+                "{\"id\":1,\"remainInResidence\":false,\"needSupport\":false,\"experienceSymptoms\":false,\"supportType\":null,\"name\":null,\"email\":null}")));
     }
-//
-//    @Test
-//    public void getQuestionnaireTest() throws Exception {
-//        RequestBuilder request = MockMvcRequestBuilders.get("/questionnaire");
-//        mvc.perform(request).andDo(print()).andExpect(content().string(containsString("COVID-19 Questionaire")));
-//
-//    }
-//
-//    @Test
-//    public void postQuestionnaireTest() throws Exception {
-//        RequestBuilder request = MockMvcRequestBuilders.post("/questionnaire");
-//        mvc.perform(request).andDo(print()).andExpect(content().string(equalTo(
-//                "{\"id\":1,\"remainInResidence\":false,\"needSupport\":false,\"experienceSymptoms\":false,\"supportType\":null,\"name\":null,\"email\":null}")));
-//    }
 
-//    @Test
-//    public void repoEamilSearchTest() {
-//        Questionnaire ques = new Questionnaire("Jake","jake@gmail.com",false,false,null,false);
-//        QR.save(ques);
-//        Questionnaire result = QR.findByEmail("jake@gmail.com");
-//        assertEquals("Jake",ques.getName());
-//    }
+    @Test
+    public void repoEamilSearchTest() {
+        Questionnaire ques = new Questionnaire("Jake","jake@gmail.com",false,false,null,false);
+        QR.save(ques);
+        Questionnaire result = QR.findByEmail("jake@gmail.com");
+        assertEquals("Jake",ques.getName());
+    }
 
-//    @Test
-//    public void getResultTest() throws Exception {
-//        RequestBuilder request = MockMvcRequestBuilders.post("/result");
-//        mvc.perform(request).andDo(print()).andExpect(content().string(equalTo(
-//                "")));
-//    }
+    @Test
+    public void getResultTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.post("/result");
+        mvc.perform(request).andDo(print()).andExpect(content().string(equalTo(
+                "")));
+    }
 
 }
